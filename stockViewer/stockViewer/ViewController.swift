066ab5c8a0a27e8197820e077490cccc8908e9b9
HenryLin <kon345@gmail.com>
2024-01-16 11:19:56 +0800

    create xcode project

---
+++ stockViewer/stockViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockViewer
+//
+//  Created by 林裕和 on 2024/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
